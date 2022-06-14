7b0e919069a29795f440e8b13ad23f2146544429
geekbrains <info@geekbrains.ru>
2022-06-14 22:31:04 +0400

    Task13

---
+++ Task13_3digit_of_number/Program.cs
@@ -0,0 +1,47 @@
+﻿int number = Input("Input number: ");
+
+if(number < 1000 && number > 99)
+{
+    int digit3 = ThirdNumFunc(number);
+    Console.WriteLine("Third  digit = " + digit3);
+}
+else
+{
+    if(number > 999)
+    {
+        int number3 = DivToThree(number);
+        int digit3 = ThirdNumFunc(number3);
+        Console.WriteLine("Third  digit = " + digit3);
+    }
+    else
+    {
+        if(number < 100)
+        {
+            Console.WriteLine("Third digit in your number is missing");
+        }
+    }
+}
+
+
+int DivToThree(int a)
+{
+    while(a > 999)
+    {
+        int b = a / 10;
+        a = b;
+    }
+    return b;
+}
+
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ThirdNumFunc(int a)
+{
+    int num = a % 10;
+    return num;
+}
