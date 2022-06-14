97f80003a7b82c023b621bcc54bcfc29479b0bb5
geekbrains <info@geekbrains.ru>
2022-06-14 21:52:08 +0400

    Task10

---
+++ Task10_second_of_three/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = Input("Input number: ");
+
+if(number > 99 && number < 1000)
+{
+    int secnum = SecNumFunc(number);
+    Console.WriteLine("Second digit = " + secnum);
+}
+else
+{
+    Console.WriteLine("You input incorrect number");
+}
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SecNumFunc(int a)
+{
+    int num = a / 10;
+    int sec = num % 10;
+    return sec;
+}
