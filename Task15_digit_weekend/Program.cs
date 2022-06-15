c5146088a902bd9cd2902d90afd20cb629f61840
geekbrains <info@geekbrains.ru>
2022-06-15 14:27:22 +0400

    task15_full_func

---
+++ Task15_digit_weekend/Program.cs
@@ -0,0 +1,31 @@
+﻿int daynum = Input("Input week's day number: ");
+
+Procedure(daynum);
+
+
+
+void Procedure(int daynum)      // Основной алгоритм (процедура)
+{
+    if(daynum < 8 && daynum > 0)
+    {
+        if(daynum > 5)
+        {
+            Console.WriteLine("Yes, this day is weekend");
+        }
+        else
+        {
+            Console.WriteLine("No, this day is weekday");
+        }
+    }
+    else
+    {
+        Console.WriteLine("You input incorrect number");
+    }
+}
+
+
+int Input(string output)        // Считывание введённого числа (функция)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
